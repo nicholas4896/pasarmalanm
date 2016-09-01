3bd55551ee3dabdb8e27652e9224ad57ac777df7
Nicholas4896 <nicholasyhs@gmail.com>
2016-09-01 11:34:34 +0800

    searching

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,3 +1,12 @@
 class TicketsController < ApplicationController
-def index
+
+  def index
+    @tickets = Ticket.all
+    if params[:search]
+      @tickets = Ticket.search(params[:search])
+    else
+      @tickets = Ticket.all
+    end
+  end
+
 end
